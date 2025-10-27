b327bf813eccdd3605119f334ad1de3efeb3a11d
Brandon Silvi <brandon.silvi@gmail.com>
2025-10-26 19:16:11 -0700

    checkerboard construction using alternatting #,and spaces with a size of 8 across.

--- script3.js
+++ script3.js
@@ -9,3 +9,20 @@
 //  # # # #
 // # # # #
 //  # # # #
+let size = 8;
+let row = "";
+
+console.log("8x8 Grid Pattern using # and space to display chessboard");
+
+for (let i = 1; i < size; i++) {
+    line = "";
+
+    for let j = 1; j <= size; j++) {
+        if ((i + j) % 2 == 0) {
+            line = line + #;
+        } else {
+            line = line +" ";
+        }
+    }
+    console.log(line);
+}
